e19f37698c0ea87ef072f594e18a55098c784bac
Joshua <joshua@brandforge.me>
2025-03-31 14:47:16 -0400

    Added the HTML for a contact form.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,33 @@
 export default function Contact() {
   return (
     <>
-      {/* About page content */}
+      <form>
+      <label>Name</label>
+      <input type="text"></input>
+      <label>Company Name</label>
+      <input type="text"></input>
+      <label>Email</label>
+      <input type="email"></input>
+      <label>Phone</label>
+      <input type="tel"></input>
+      <label>Project Description</label>
+      <textarea name="projectDescription" rows="7" cols="10"></textarea>
+      <div>
+        <button>
+          $5K-25K
+        </button>
+        <button>
+          $25K-50K
+        </button>
+        <button>
+          $50K-100K
+        </button>
+        <button>
+          $100K+
+        </button>
+      </div>
+      <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
