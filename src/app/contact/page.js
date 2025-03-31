7e5af13638d73692c997990c8b99685054718a99
Joshua <joshua@brandforge.me>
2025-03-31 15:52:04 -0400

    Final

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,18 +4,18 @@ export default function Contact() {
       <form>
        <div> 
         <label for="userName">Name</label>
-        <input type="text" name="userName" placeholder="John Doe"/>
+        <input type="text" id="userName" placeholder="John Doe"/>
         <label for="companyName">Company Name</label>
-        <input type="text" name="companyName" placeholder="Brandforge"/>
+        <input type="text" id="companyName" placeholder="Brandforge"/>
       </div>
       <div>
         <label for="email">Email</label>
-        <input type="email" name="email" placeholder="johndoe@company.mail"/>
+        <input type="email" id="email" placeholder="johndoe@company.mail"/>
         <label for="phoneNumber">Phone</label>
-        <input type="tel" name="phoneNumber" placeholder="5-(555)-555-5555"/>
+        <input type="tel" id="phoneNumber" placeholder="5-(555)-555-5555"/>
       </div>
       <label for="projectDescription">Project Description</label>
-      <textarea name="projectDescription" rows="4" cols="50" placeholder="I want to make a simple website for my company that will help me grow my business."></textarea>
+      <textarea id="projectDescription" rows="4" cols="50" placeholder="I want to make a simple website for my company that will help me grow my business."></textarea>
       <div>
         <button>
           $5K-25K
