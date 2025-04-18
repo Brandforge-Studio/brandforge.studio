85bd48b24f91aba11032a57bffb1d69720f3ed09
Garland Key <gkey@valcom.com>
2025-04-18 00:13:48 -0400

    Adds logo.webp

--- src/app/_components/PageNav.js
+++ src/app/_components/PageNav.js
@@ -1,7 +1,10 @@
 'use client';
 import NavButton from "./NavButton";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function PageNav() {
+  const pathname = usePathname();
   const navButtons = [
     { label: "About", linkedPath: "/about" },
     { label: "Services", linkedPath: "/services" },
@@ -10,14 +13,26 @@ export default function PageNav() {
   ];
 
   return (
-    <nav className="flex flex-row gap-8 justify-end p-8 w-full">
-      {navButtons.map(button => (
-        <NavButton 
-          key={button.label}
-          label={button.label}
-          linkedPath={button.linkedPath}
-        />
-      ))}
+    <nav className="flex flex-row items-center justify-between p-8 relative w-full">
+      {
+        pathname !== "/" && (
+          <Image
+            src="/logo.webp"
+            alt="Logo"
+            width={172}
+            height={80}
+            className="absolute left-8 top-1/2 -translate-y-1/2 w-44 h-20"
+          />
+      )}
+      <div className="flex-grow flex flex-row gap-8 justify-end">
+        {navButtons.map(button => (
+          <NavButton 
+            key={button.label}
+            label={button.label}
+            linkedPath={button.linkedPath}
+          />
+        ))}
+      </div>
     </nav>
   );
 }
